3ee598a388aa1baf2ba9df13a1490b9c53a2fa26
Edo van Veen <e.n.w.vanveen@tudelft.nl>
2023-05-16 14:54:57 +0200

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='naclib',
     packages=['naclib'],
-    version='1.0.1',
+    version='1.1.0',
     license='MIT',
     description='Non-Affine Corrections on microscopy images using S/T Polynomial Decomposition',
     long_description='NAClib is a python library for applying a global alignment method ' + \
@@ -15,7 +15,7 @@ setup(
     author='Edo van Veen & Kaley McCluskey',
     author_email='edovanveen@gmail.nl',
     url='https://github.com/edovanveen/naclib',
-    download_url='https://github.com/edovanveen/naclib/blob/master/dist/naclib-1.0.1.tar.gz',
+    download_url='https://github.com/edovanveen/naclib/blob/master/dist/naclib-1.1.0.tar.gz',
     keywords=['colocalization', 'microscopy', 'distortion correction', 'STPD'],
     install_requires=[
         'scipy',
